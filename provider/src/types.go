640f2ba52c87f16d71851a639127d22cbeb2bc88
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-03-21 14:00:00 +0000

    Showcase several access points for pacts

--- provider/src/types.go
+++ provider/src/types.go
@@ -1,8 +1,11 @@
 package main
 
+// OkReponse represents an ok response
 type OkReponse struct {
 	Message string `json:"message"`
 }
+
+// ErrorResponse represents a not ok response
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
--- provider/tests/verify_test.go
+++ provider/tests/verify_test.go
@@ -7,18 +7,19 @@ import (
 	"github.com/pact-foundation/pact-go/types"
 )
 
-
 func TestProvider(t *testing.T) {
 
 	pact := &dsl.Pact{
-		Consumer: "MyConsumer",
-		Provider: "MyProvider",
+		Consumer:                 "MyConsumer",
+		Provider:                 "MyProvider",
 		DisableToolValidityCheck: true,
 	}
 
 	pact.VerifyProvider(t, types.VerifyRequest{
+		// BrokerURL:       "http://localhost",
+		// Tags:            []string{"master"},
+		PactURLs:        []string{"http://localhost/pacts/provider/MyProvider/consumer/MyConsumer/latest"},
 		ProviderBaseURL: "http://localhost:3000",
-		BrokerURL: "http://localhost",
 		StateHandlers: types.StateHandlers{
 			"default": func() error { return nil },
 		},
