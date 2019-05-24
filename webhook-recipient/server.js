d5930595616b9b7ec6be477aecef7da1b09ffac0
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-05-24 15:06:00 +0100

    Display webhook interpolated data

--- webhook-recipient/server.js
+++ webhook-recipient/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const { inspect } = require("util");
 
-app.post("/target", function(req, res) {
-  console.log("Hey");
-  res.sendStatus(200);
-});
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .post("/target", function(req, res) {
+    console.log(
+      `Hey ${req.body.consumerName} contract has changed\n${inspect(req.body, {
+        showHidden: true,
+        depth: null
+      })}`
+    );
+    res.sendStatus(200);
+  });
 
 app.listen(3001);
