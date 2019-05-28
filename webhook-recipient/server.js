af4704549653155d226dc3dc9226ac3f5f14dcb8
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-05-28 14:51:10 +0100

    Trigger a provider verification on webhook

--- webhook-recipient/server.js
+++ webhook-recipient/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const { resolve } = require("path");
 const { inspect } = require("util");
+const { spawn } = require("child_process");
 
 app
   .use(express.json())
@@ -12,7 +14,21 @@ app
         depth: null
       })}`
     );
-    res.sendStatus(200);
+    const ls = spawn("make", ["verify"], {
+      cwd: resolve(process.cwd(), "../")
+    });
+    ls.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on("data", data => {
+      console.log(`stderr: ${data}`);
+    });
+
+    ls.on("exit", code => {
+      console.log(`child process exited with code ${code}`);
+      res.sendStatus(200);
+    });
   });
 
 app.listen(3001);
