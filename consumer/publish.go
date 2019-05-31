7a55799f399ef259bd42cd6c523504d9a83de648
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-05-31 16:23:52 +0100

    Add package version to consumer version

--- consumer/publish.go
+++ consumer/publish.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/pact-foundation/pact-go/dsl"
 	"github.com/pact-foundation/pact-go/types"
 )
 
+// PackageJSON is package.json struct
+type PackageJSON struct {
+	Version string `json:"version"`
+}
+
 func main() {
 
+	revision := getRevision()
+	version := getPackageVersion()
+	consumerVersion := strings.TrimSpace(version + "-" + revision)
+	p := dsl.Publisher{}
+
+	err := p.Publish(types.PublishRequest{
+		PactURLs:        []string{"../consumer/pacts/pactconsumer_frontend-pactprovider_backend.json"},
+		PactBroker:      "http://localhost:80",
+		ConsumerVersion: consumerVersion,
+		Tags:            []string{"master"},
+	})
+
+	if err != nil {
+		log.Println("ERROR: ", err)
+		os.Exit(1)
+	}
+
+}
+
+func getRevision() string {
 	revision, err := exec.Command(`git`, []string{"rev-parse", "HEAD"}...).Output()
 
 	if err != nil {
@@ -20,17 +48,19 @@ func main() {
 	}
 	fmt.Printf("%s\n", revision)
 
-	p := dsl.Publisher{}
-	nerr := p.Publish(types.PublishRequest{
-		PactURLs:        []string{"../consumer/pacts/pactconsumer_frontend-pactprovider_backend.json"},
-		PactBroker:      "http://localhost:80",
-		ConsumerVersion: string(revision),
-		Tags:            []string{"master"},
-	})
+	return string(revision)
+}
 
-	if nerr != nil {
-		log.Println("ERROR: ", err)
-		os.Exit(1)
+func getPackageVersion() string {
+	jsonFile, err := os.Open("package.json")
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
+	var pkg PackageJSON
+	json.Unmarshal(byteValue, &pkg)
+
+	return pkg.Version
 }
